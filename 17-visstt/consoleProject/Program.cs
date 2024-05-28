f6b5e4fa5052db911bf5c0ef5322cd7f7a008359
visstt <123978467+visstt@users.noreply.github.com>
2024-05-28 14:52:48 +0500

    Add files via upload

---
+++ 17-visstt/consoleProject/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace consoleProject
+{
+    /*
+     * ФИО студента: Скоморохов Егор Константинович
+     * номер варианта: 20
+     * условие задачи (скопировать из листа задания): Создать класс Subject описывающий предмет для изучения как сущность в приложении
+     */
+    using System;
+    using System.Collections.Generic;
+
+    namespace consoleProject
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                ISubject subject1 = new SubjectImplementation("Основы алгоритмизации и программирования", 120, ControlType.Exam);
+                subject1.PrintInfo();
+                Console.WriteLine();
+                ISubject subject2 = new SubjectImplementation("Мдк 04.02", 150, ControlType.Exam);
+                subject2.PrintInfo();
+                Console.WriteLine();
+                ISubject subject3 = new SubjectImplementation("Физическая культура", 70, ControlType.Test);
+                subject3.PrintInfo();
+
+                Console.WriteLine();
+                Console.WriteLine("Функционал IClonable");
+                Console.WriteLine();
+
+                ISubject subject4 = (ISubject)subject1.Clone();
+                subject4.PrintInfo();
+
+                Console.WriteLine();
+                Console.WriteLine("Функционал IComparable");
+                Console.WriteLine();
+
+                List<ISubject> subjects = new List<ISubject>
+            {
+                new SubjectImplementation("Основы алгоритмизации и программирования", 120, ControlType.Exam),
+                new SubjectImplementation("Мдк 04.02", 150, ControlType.Exam),
+                new SubjectImplementation("Физическая культура", 70, ControlType.Test),
+                new SubjectImplementation("Математика", 100, ControlType.Exam),
+                new SubjectImplementation("Информатика", 130, ControlType.Exam),
+                new SubjectImplementation("Литература", 80, ControlType.Test),
+                new SubjectImplementation("История", 90, ControlType.Exam),
+                new SubjectImplementation("Физика", 110, ControlType.Exam),
+                new SubjectImplementation("Химия", 140, ControlType.Exam),
+                new SubjectImplementation("Биология", 105, ControlType.Test)
+            };
+
+                subjects.Sort();
+                Console.WriteLine("Сортированный список:");
+                foreach (var subject in subjects)
+                {
+                    subject.PrintInfo();
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Функционал IDisposable");
+                Console.WriteLine();
+
+                using (ISubject subject = new SubjectImplementation("Пример предмета", 50, ControlType.Exam))
+                {
+                    subject.PrintInfo();
+                }
+            }
+        }
+    }
+}
---
+++ 17-visstt/consoleProject/Subject.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleProject
+{
+    enum ControlType { Exam, Test }
+
+    // Интерфейс ISubject
+    public interface ISubject : ICloneable, IComparable<ISubject>, IDisposable
+    {
+        string Title { get; set; }
+        int Duration { get; set; }
+
+
+        void PrintInfo();
+        void RegisterSubject();
+        void RemoveSubject();
+    }
+
+    // Реализация интерфейса ISubject
+    internal class SubjectImplementation : ISubject
+    {
+        public string Title { get; set; }
+        public int Duration { get; set; }
+        public ControlType ControlType { get; set; }
+
+        public SubjectImplementation(string title, int duration, ControlType controlType)
+        {
+            this.Title = title;
+            this.Duration = duration;
+            this.ControlType = controlType;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(
+                $"Название предмета: {Title} \n" +
+                $"Продолжительность в часах: {Duration} \n" +
+                $"Вид экзамена: {ControlType}");
+        }
+
+        public void RegisterSubject()
+        {
+            Console.WriteLine($"Предмет '{Title}' был зарегистрирован.");
+        }
+
+        public void RemoveSubject()
+        {
+            Console.WriteLine($"Предмет '{Title}' был удален.");
+        }
+
+        public object Clone()
+        {
+            return new SubjectImplementation(this.Title, this.Duration, this.ControlType);
+        }
+
+        public int CompareTo(ISubject other)
+        {
+            if (other == null) return 1;
+            return this.Duration.CompareTo(other.Duration);
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine($"Ресурсы предмета '{Title}' были освобождены.");
+        }
+    }
+}
